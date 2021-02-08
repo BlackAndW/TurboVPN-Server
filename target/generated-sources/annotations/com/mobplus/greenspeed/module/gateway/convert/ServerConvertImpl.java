7c4659e1fb09792d3a890dbc678e57497374eb57
BlackAndW <2586127609@qq.com>
2021-02-08 16:35:35 +0800

    配置文件修改

--- target/generated-sources/annotations/com/mobplus/greenspeed/module/gateway/convert/ServerConvertImpl.java
+++ target/generated-sources/annotations/com/mobplus/greenspeed/module/gateway/convert/ServerConvertImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-02-04T09:32:01+0800",
+    date = "2021-02-04T10:32:11+0800",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 1.8.0_231 (Oracle Corporation)"
 )
 @Component
