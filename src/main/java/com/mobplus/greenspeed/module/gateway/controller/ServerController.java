2a06beb010793b74db1bf38df7ec2831791ff909
BlackAndW <2586127609@qq.com>
2021-08-04 14:40:29 +0800

    Update ServerController.java
    下划线转驼峰（默认下划线的原因未知。。。）

--- src/main/java/com/mobplus/greenspeed/module/gateway/controller/ServerController.java
+++ src/main/java/com/mobplus/greenspeed/module/gateway/controller/ServerController.java
@@ -1,6 +1,9 @@
 package com.mobplus.greenspeed.module.gateway.controller;
 
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.PropertyNamingStrategy;
+import com.alibaba.fastjson.serializer.SerializeConfig;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.mobplus.greenspeed.Constants;
@@ -186,7 +189,11 @@ public class ServerController {
         query.put("clearCache", true);
         List<Server> list = serverService.query(query);
         List<ServerVO> resultList = transform(list, "en");
-        return Result.SUCCESS(resultList);
+        // 下划线转驼峰，（默认下划线的原因未知）
+        SerializeConfig config = new SerializeConfig();
+        config.propertyNamingStrategy = PropertyNamingStrategy.CamelCase;
+        String response = JSON.toJSONString(resultList, config);
+        return Result.SUCCESS(response);
     }
 
     @PostMapping("/create")
